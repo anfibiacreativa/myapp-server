6bdc2dd45848a2161622126b897818852f1f2ee6
Natalia Venditto <natalia.venditto@netcentric.biz>
2019-02-24 18:56:39 +0100

    changing config

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
-var express = require('express');
-var express_graphql = require('express-graphql');
-var { buildSchema } = require('graphql');
+const express = require('express');
+const express_graphql = require('express-graphql');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const { buildSchema } = require('graphql');
 
 // Schema for our GraphQL validation
-var schema = buildSchema(`
+const schema = buildSchema(`
     type Query {
         message: String
         kitten(id: String!): Kitten,
@@ -15,9 +17,9 @@ var schema = buildSchema(`
     }
 `);
 
-var getKitten = function(args) {
+const getKitten = function(args) {
     if (args.id) {
-        var kitten = args.id;
+        const kitten = args.id;
         return kittenData.filter(kitten => kitten.id === id);
     } else {
         return kittenData;
@@ -25,12 +27,12 @@ var getKitten = function(args) {
 }
 
 // Resolves the root of your API
-var root = {
+const root = {
     message: () => 'API Server ready!',
     kittens: getKitten
 };
 
-var kittenData = [
+const kittenData = [
     {
         id: '1',
         name: 'pedro'
@@ -56,12 +58,28 @@ var kittenData = [
         name: 'gazu'
     }
 ]
-
+// Define cors options to be able to use cross doamin resource sharing
+const whitelist = ['http://localhost:8001']
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true)
+    } else {
+      callback(new Error('Not allowed by CORS!!!'))
+    }
+  },
+  credentials: true
+}
 // Starts up the server and defines the port to reach the endpoint, returning `message` at the root
-var app = express();
+const app = express().use(cors(corsOptions)).use(bodyParser.json());
 app.use('/api', express_graphql({
     schema: schema,
     rootValue: root,
     graphiql: true
 }));
-app.listen(8000, () => console.log('GraphQL server is up and running on localhost:8000/api'));
+app.listen(8000, err => {
+    if (err) {
+      return console.log(err);
+    }
+    return console.log('GraphQL server is up and running on localhost:8000/api');
+});
