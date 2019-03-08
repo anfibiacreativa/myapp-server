978011d94b7ad14eb0231753ad0b19735ab85aba
Natalia Venditto <natalia.venditto@netcentric.biz>
2019-03-08 08:56:00 +0100

    change to renderer

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express_graphql = require('express-graphql');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const { buildSchema } = require('graphql');
-const CATS = require('./cats');
+// import CATS from './cats';
 
 // Schema for our GraphQL validation
 const schema = buildSchema(`
@@ -14,13 +14,10 @@ const schema = buildSchema(`
     }
     type Kitten {
         id: String,
-        name: String,
-        mission: String
+        name: String
     }
 `);
 
-// get the kitten data
-const kittenData = CATS;
 const getKitten = function(args) {
     if (args.id) {
         const kitten = args.id;
@@ -36,6 +33,98 @@ const root = {
     kittens: getKitten
 };
 
+const kittenData = [
+  {
+      id: '1',
+      name: 'pedro',
+      mission: 'apollo'
+  },
+  {
+      id: '2',
+      name: 'pablo',
+      mission: 'apollo'
+  },
+  {
+      id: '3',
+      name: 'vilma',
+      mission: 'apollo'
+  },
+  {
+      id: '4',
+      name: 'betty',
+      mission: 'sputnik'
+  },
+  {
+      id: '5',
+      name: 'slate',
+      mission: 'apollo'
+  },
+  {
+      id: '6',
+      name: 'gazu',
+      mission: 'sputnik'
+  },
+  {
+      id: '7',
+      name: 'fabio',
+      mission: 'sputnik'
+  },
+  {
+      id: '8',
+      name: 'dani',
+      mission: 'sputnik'
+  },
+  {
+      id: '9',
+      name: 'paco',
+      mission: 'sputnik'
+  },
+  {
+      id: '10',
+      name: 'gru',
+      mission: 'sputnik'
+  },
+  {
+      id: '11',
+      name: 'fabio',
+      mission: 'tatiana'
+  },
+  {
+      id: '12',
+      name: 'claudio',
+      mission: 'apollo'
+  },
+  {
+      id: '13',
+      name: 'tatiana',
+      mission: 'sputnik'
+  },
+  {
+      id: '14',
+      name: 'armando',
+      mission: 'sputnik'
+  },
+  {
+      id: '15',
+      name: 'serafin',
+      mission: 'apollo'
+  },
+  {
+      id: '16',
+      name: 'marcos',
+      mission: 'sputnik'
+  },
+  {
+      id: '17',
+      name: 'martina',
+      mission: 'sputnik'
+  },
+  {
+      id: '18',
+      name: 'alejo',
+      mission: 'sputnik'
+  }
+]
 // Define cors options to be able to use cross doamin resource sharing
 const whitelist = ['http://localhost:8001', 'http://localhost:8000/api']
 const corsOptions = {
